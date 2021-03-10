7a2bb347e756c65777be20ccd123c2f364e5a267
turanmerter <turanmerter@gmail.com>
2021-03-10 22:57:02 +0300

    some data control added for API result

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ This application is a simple prototype that uses Spotify Web API to gather the m
 
 There is also a front end element baked into it. It is developed using template feature of the flask.
 
-The project uses two package: flask and requests.
+The project uses two packages: flask and requests.
 
 In order to start the server, credential.json file must be filled with a valid spotify developer account information. After that, running start.bat is enough.
--- spotifyapp/spotify.py
+++ spotifyapp/spotify.py
@@ -25,6 +25,7 @@ class Spotify:
     _expiration_usage_perc = 0.9
 
     def read_credential(self):
+        # reading from the environmant variables would be a better idea
         with open(self._credential_file_name, "r") as credential_file:
             credential = credential_file.read()
         credential_dictionary = json.loads(credential)
@@ -47,8 +48,10 @@ class Spotify:
     def _get_access_token(self):
         body={"grant_type":"client_credentials"}
         response = requests.post(self._access_token_url, data=body, headers=self._get_header_with_basic_auth())
-        response_json = response.json()
+        if response.status_code != 200:
+            raise Exception("Spotify returned an error for some reason")
 
+        response_json = response.json()
         self._access_token = response_json["access_token"]
         expires_in = response_json["expires_in"]
         self._expires_at = datetime.datetime.now() + datetime.timedelta(0, int(expires_in * self._expiration_usage_perc)) 
@@ -64,11 +67,25 @@ class Spotify:
     def _search_for_artist_id(self, artist_name):
         params={"q": artist_name, "type": "artist", "limit": 1}
         response = requests.get(self._search_url, params=params, headers=self._get_header_with_access_token())
+        if response.status_code != 200:
+            raise Exception("Spotify returned an error for some reason")
+
         response_json = response.json()
         self._artist_id_dict[artist_name] = self._parse_search_response(response_json)
 
     def _parse_search_response(self, response_json):
-        return response_json["artists"]["items"][0]["id"]
+        artists = response_json.get("artists")
+        if artists is None:
+            raise Exception("Artist could not be found")
+
+        items = artists.get("items")
+        if artists is None:
+            raise Exception("Artist could not be found")
+
+        if len(items) <= 0:
+            raise Exception("Artist could not be found")
+
+        return items[0].get("id")
 
     def _get_artist_id(self, artist_name):
         if artist_name not in self._artist_id_dict:
@@ -80,6 +97,9 @@ class Spotify:
         params={"market": "TR"} # just an assumption, can be configurable
         artist_id = self._get_artist_id(artist_name)
         response = requests.get(self._top_tracks_url.format(artist_id), params=params, headers=self._get_header_with_access_token())
+        if response.status_code != 200:
+            raise Exception("Spotify returned an error for some reason")
+
         response_json = response.json()
 
         return self._parse_most_popular_response(response_json)
@@ -87,14 +107,32 @@ class Spotify:
     def _parse_most_popular_response(self, response_json):
         songs = list()
 
-        tracks = response_json["tracks"]
+        songs_counter = 0
+        tracks = response_json.get("tracks", [])
         for track in tracks:
-            album = track["album"]
-            artist = track["artists"][0]
-            songs.append(Song(artist["name"], track["name"], album["images"][0]["url"], album["release_date"]).__dict__)
+            if songs_counter >= 5:
+                break
 
-        return songs[:5]
+            album = track.get("album")
+            artists = track.get("artists", [])
 
+            artist_name = "-"
+            album_release_date = "unknown"
+            album_image_url = ""
+            track_name = track.get("name", "unknown")
 
+            if len(artists) > 0:
+                artist = artists[0]
+                artist_name = artist.get("name", "-")
+            
+            if album is not None:
+                album_release_date = album.get("release_date", "unknown")
+                album_images = album.get("images")
+                if album_images is not None:
+                    if len(album_images) > 0:
+                        album_image_url = album_images[0].get("url", "")
 
+            songs.append(Song(artist_name, track_name, album_image_url, album_release_date).__dict__)
+            songs_counter = songs_counter + 1
 
+        return songs
